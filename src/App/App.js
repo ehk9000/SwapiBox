6d4b15e3daff7bee391856b7aed75d95a777c860
Erik Konner <erikkonner@gmail.com>
2019-05-01 23:31:05 -0600

    File Structure

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import OpeningTitle from '../OpeningTitle/OpeningTitle'
+import MainContainer from '../MainContainer/MainContiner'
+
+function App() {
+    return (
+      <section className="App">
+        <OpeningTitle />
+        <MainContainer />
+      </section>
+    )
+}
+
+export default App;
